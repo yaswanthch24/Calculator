d938d3545173cd628a46e635ffff1ded3fd9a624
yaswanth chanumolu <chanumoluyaswanth24@gmail.com>
2024-01-17 21:10:26 +0530

    Completed code

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function dis(val) { 
     document.getElementById("result").value += val 
 } 
@@ -24,47 +23,14 @@ cal.onkeyup = function (event) {
     } 
 } 
 
+// Function that evaluates the digit and return result 
 function solve() { 
     let x = document.getElementById("result").value 
     let y = math.evaluate(x) 
     document.getElementById("result").value = y 
 } 
 
+// Function that clear the display 
 function clr() { 
     document.getElementById("result").value = "" 
-=======
-function dis(val) { 
-    document.getElementById("result").value += val 
-} 
-
-function myFunction(event) { 
-    if (event.key == '0' || event.key == '1' 
-        || event.key == '2' || event.key == '3' 
-        || event.key == '4' || event.key == '5' 
-        || event.key == '6' || event.key == '7' 
-        || event.key == '8' || event.key == '9' 
-        || event.key == '+' || event.key == '-' 
-        || event.key == '*' || event.key == '/') 
-        document.getElementById("result").value += event.key; 
-} 
-
-var cal = document.getElementById("calcu"); 
-cal.onkeyup = function (event) { 
-    if (event.keyCode === 13) { 
-        console.log("Enter"); 
-        let x = document.getElementById("result").value 
-        console.log(x); 
-        solve(); 
-    } 
-} 
-
-function solve() { 
-    let x = document.getElementById("result").value 
-    let y = math.evaluate(x) 
-    document.getElementById("result").value = y 
-} 
-
-function clr() { 
-    document.getElementById("result").value = "" 
->>>>>>> e780f8f13a315654c34347ef917fa46676ad276c
 } 
